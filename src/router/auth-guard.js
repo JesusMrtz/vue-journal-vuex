4672989ec1f07a811edbc6cd880edc8f919d5906
jmartinez <jmartinezto@minsait.com>
2022-07-20 08:30:27 -0500

    check login

--- src/router/auth-guard.js
+++ src/router/auth-guard.js
@@ -3,10 +3,7 @@ import store from "@/store"
 const isAuthenticatedGuard = async ( to, from, next ) => {
   const response =  await store.dispatch('auth/checkAuthentication')
 
-  // console.log(response)
-  // next()
-  // return
-
+  // Cuando inicio sesión me envia un undefined y no se porqué, pero cuando no estoy logueado me manda un ok : false
   if ( response === undefined ) next()
   else next({ name: 'login' })
 }
